f768128dae2926d0f981dcd82f92960be47faf02
Nathaniel Anum Adjah <nathanielanum13@gmail.com>
2023-02-07 16:18:45 +0000

    new changes

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JSON Schema Validation Files</title>
+</head>
+<body>
+    <h1>Welcome to Grey Schema Validation</h1>
+    <?php
+        echo "Here are our files";
+        $path = ".";
+        $dh = opendir($path);
+        $i=1;
+        while (($file = readdir($dh)) !== false) {
+            if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+                echo "<a href='$path/$file'>$file</a><br /><br />";
+                $i++;
+            }
+        }
+        closedir($dh);
+    ?> 
+</body>
+</html>
