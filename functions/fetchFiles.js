f7619d3fe35f8fb3f971cce5ee8245a92f45cc4e
Nathaniel Anum Adjah <nathanielanum13@gmail.com>
2023-02-08 15:33:56 +0000

    minor changes

--- functions/fetchFiles.js
+++ functions/fetchFiles.js
@@ -5,14 +5,23 @@ const dir = '../';
 exports.handler = async (event) => {
     const { targetDir } = JSON.parse(event.body)
     let files = []
-    try {
-        files = fs.readdirSync(dir + targetDir)
-    } catch {
+
+    if (fs.lstatSync(dir + targetDir).isDirectory()) {
+        try {
+            files = fs.readdirSync(dir + targetDir)
+        } catch {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: "Failed to read directory" })
+            }
+        }
+    } else {
         return {
-            statusCode: 400,
-            body: JSON.stringify({ message: "Failed to read directory" })
+            statusCode: 200,
+            body: require(dir + targetDir)
         }
     }
+    
     return {
         statusCode: 200,
         body: JSON.stringify({ data: files })
